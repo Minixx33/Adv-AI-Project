9b813f93baa6561f4f5e0314a2c950ab1c5fa502
Yasmine <g00084287@aus.edu>
2026-05-05 23:56:53 +0400

    cleanup

--- Taky (ft Claude) Model/werewolf/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from werewolf.agents.random_agent import RandomAgent
-from werewolf.agents.heuristic_agent import HeuristicAgent
-from werewolf.agents.bayesian_agent import BayesianAgent
-from werewolf.agents.suspicion_agent import SuspicionAgent
